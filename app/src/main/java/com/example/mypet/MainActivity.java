066dd0c686936699736a054f01eff55b113afdcf
2869347920@qq.com <2869347920@qq.com>
2023-10-04 22:48:40 +0800

    fix

--- app/src/main/java/com/example/mypet/MainActivity.java
+++ app/src/main/java/com/example/mypet/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
