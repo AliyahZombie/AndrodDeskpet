7148160556eac82953c75bbf204b6e039368a10a
2869347920@qq.com <2869347920@qq.com>
2023-10-04 22:59:33 +0800

    fix

--- app/src/main/java/com/example/mypet/MainActivity.java
+++ app/src/main/java/com/example/mypet/MainActivity.java
@@ -362,7 +362,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case "鹿弈" :
-                if (age < 60) {
+                if (pet_age < 60) {
                     pet_img = luyiSet[0];
                 } else if (age < 3600) {
                     pet_img = luyiSet[1];
